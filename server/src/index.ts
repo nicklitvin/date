b2b5d29e2622132ab595af60417e30c98aceb298
nicklitvin <nick.litvinov9@gmail.com>
2024-01-17 19:43:36 -0800

    set up postgres

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,25 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
+
+
+const x = async () => {
+    await prisma.user.create({
+        data: {
+            id: 1,
+            name: "nick"
+        }
+    })
+    console.log("asd");
+}
+x();
+
 app.get("/", (req, res) => {
     res.json("hi");
 })
+
+
 
 app.listen(3000);
