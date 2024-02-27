162f7b57cd695573e3b8e272b0b37bdc079bcf92
nicklitvin <nick.litvinov9@gmail.com>
2024-02-27 14:37:34 -0800

    add page header

--- frontend/src/components/PageHeader.tsx
+++ frontend/src/components/PageHeader.tsx
@@ -1,30 +1,55 @@
 import { Image } from "expo-image"
-import { StyledText, StyledView } from "../styledElements"
+import { StyledImage, StyledText, StyledView } from "../styledElements"
+
+type ImageName = "SendTriangle";
 
 interface Props {
     title: string
-    imageSource: string
+    imageSource?: string
     rightContent?: React.JSX.Element
     swapTitleAndImage? : boolean
+    imageType?: ImageName
 }
+
 export function PageHeader(props : Props) {
+    const SendTriangle = require(`../../assets/SendTriangle.png`);
+
+    let imageData;
+    switch (props.imageType) {
+        case ("SendTriangle"): imageData = SendTriangle; break;
+    }
+
+    const imageElement = props.imageSource ? 
+        <StyledImage 
+            className="w-[75px] h-[75px] rounded-full"
+            source={{uri : props.imageSource}}
+        /> : 
+        <StyledImage 
+            className="w-[25px] h-[25px]"
+            source={imageData}
+        />
+        
+    const textElement = <StyledText className="text-3xl font-bold">
+        {props.title}
+    </StyledText>
+    const middle = <StyledView className="w-3"/>
+
     return (
-        <StyledView>
+        <StyledView className="w-full flex flex-row bg-danger items-center">
             {
             props.swapTitleAndImage ?
             <>
-                <Image source={props.imageSource}/>
-                <StyledText>
-                    {props.title}
-                </StyledText>
+                {imageElement}
+                {middle}
+                {textElement}
             </> :
             <>
-                <StyledText>
-                    {props.title}
-                </StyledText>
-                <Image source={props.imageSource}/>
+                {textElement}
+                {middle}
+                {imageElement}
             </>
             }
+            <StyledView className="flex-grow"/>
             {props.rightContent}
         </StyledView>
     )
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,11 +2,41 @@ import { observer } from "mobx-react-lite";
 import { MyTextInput } from "../components/TextInput";
 import { ChatPreviewBox } from "../components/ChatPreviewBox";
 import { makePublicProfile, makeReceivedMessage, makeSentMessage } from "../../__testUtils__/easySetup";
+import { PageHeader } from "../components/PageHeader";
+import { StyledImage, StyledView } from "../styledElements";
 
 export function Home() {
     return (
         <>
-            <ChatPreviewBox
+            {/* <PageHeader
+                title="Page"
+                imageSource=""
+                imageType="SendTriangle"
+            />
+            <PageHeader
+                title="Page"
+                imageSource=""
+                imageType="SendTriangle"
+                swapTitleAndImage={true}
+            />
+            <PageHeader
+                title="Page"
+                imageSource="https://pbs.twimg.com/profile_images/1262372966073016321/DH4rOj9S_400x400.jpg"
+                swapTitleAndImage={true}
+            />
+            <PageHeader
+                title="Page"
+                imageSource="https://pbs.twimg.com/profile_images/1262372966073016321/DH4rOj9S_400x400.jpg"
+                swapTitleAndImage={true}
+                rightContent={
+                    <StyledImage
+                        className="w-[20px] h-[20px]"
+                        source={require("../../assets/SendTriangle.png")}
+                    />
+                }
+            /> */}
+
+            {/* <ChatPreviewBox
                 chatPreview={{
                     profile: makePublicProfile("id"),
                     messages: [makeSentMessage()]
@@ -23,7 +53,7 @@ export function Home() {
                     profile: makePublicProfile("id"),
                     messages: [makeReceivedMessage("id",new Date(), false)]
                 }}
-            />
+            /> */}
         </>
     )
 }
